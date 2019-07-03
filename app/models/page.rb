934c35f2496a793c35cf5d90c5cc5fe4b57273bd
Jonathan Abbett <jonathan@act.md>
2019-07-03 19:58:07 -0400

    Feature: Per-page file attachments

--- app/models/page.rb
+++ app/models/page.rb
@@ -7,6 +7,7 @@ class Page < ApplicationRecord
   belongs_to :creator, class_name: "User"
   belongs_to :updater, class_name: "User", optional: true
   has_many :comments
+  has_many_attached :attachments
 
   validates :title, presence: true
 end
